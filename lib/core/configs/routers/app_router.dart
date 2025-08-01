0b3904ef62969a64f56001e476960973998d06b9
Augusta Phan <pxn.na6923@gmail.com>
2025-08-02 02:58:05 +0700

    init order page

--- lib/core/configs/routers/app_router.dart
+++ lib/core/configs/routers/app_router.dart
@@ -16,6 +16,7 @@ import 'package:revive_flutter_project/features/home/bloc/branch_bloc/branch_blo
 import 'package:revive_flutter_project/features/home/bloc/home_bloc/home_bloc.dart';
 import 'package:revive_flutter_project/features/home/presentation/branches_page.dart';
 import 'package:revive_flutter_project/features/home/presentation/home_page.dart';
+import 'package:revive_flutter_project/features/order/presentation/order_page.dart';
 import 'package:revive_flutter_project/features/person/bloc/change_password/change_password_bloc.dart';
 import 'package:revive_flutter_project/features/person/bloc/user_management/user_management_bloc.dart';
 import 'package:revive_flutter_project/features/person/presentation/admin/user_management_page.dart';
@@ -73,6 +74,13 @@ final GoRouter routers = GoRouter(
               ..add(const ProductEvent.fetchAllCategoriesAndProducts()),
             child: const ProductPage(),
           ),
+        ),
+        GoRoute(
+          name: "order-page",
+          path: '/order',
+          builder: (context, state) {
+            return const OrderPage();
+          },
         ),
         GoRoute(
           name: 'person-page',
---
+++ lib/features/order/presentation/order_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:revive_flutter_project/core/constants/strings.dart';
+import 'package:revive_flutter_project/core/constants/ui_values.dart';
+import 'package:revive_flutter_project/core/widgets/my_appbar.dart';
+import 'package:revive_flutter_project/core/widgets/my_icon_button.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppbar(
+        title: "",
+        isLeadingImplied: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: MyIconButton(
+              icon: addIcon,
+              size: 30.0,
+              onTap: () {},
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: pageHorizontalPadding,
+        child: Column(
+          children: [
+            Text(
+              "ĐƠN HÀNG CỦA BẠN",
+              style: headerStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
