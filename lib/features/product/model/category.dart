1b0b242c623068fd1ac0b34389358e82b13e4f74
Augusta Phan <pxn.na6923@gmail.com>
2025-08-08 14:05:04 +0700

    done order

--- lib/features/product/model/category.dart
+++ lib/features/product/model/category.dart
@@ -10,6 +10,7 @@ class Category with _$Category {
     @JsonKey(name: 'category_name') required String name,
     @JsonKey(name: 'category_description') @Default("") String description,
     @JsonKey(name: 'category_image') @Default("") String image,
+    @JsonKey(name: 'category_amount') @Default(0.0) double amount,
   }) = _Category;
 
   factory Category.fromJson(Map<String, Object?> json) => _$CategoryFromJson(json);
