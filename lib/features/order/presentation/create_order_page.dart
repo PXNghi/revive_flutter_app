fe9cefe5a718dd82018f1b67253896df7f996948
Augusta Phan <pxn.na6923@gmail.com>
2025-08-16 00:58:39 +0700

    show user address when they create new order

--- lib/features/order/presentation/create_order_page.dart
+++ lib/features/order/presentation/create_order_page.dart
@@ -116,10 +116,49 @@ class _CreateOrderPageState extends State<CreateOrderPage> {
                       errorText: state.userPhoneError,
                     ),
                     const SizedBox(height: 16),
-                    MyTextField(
-                      label: "Địa chỉ",
+                    Row(
+                      children: [
+                        Text(
+                          "Địa chỉ",
+                          style: contentStyle.copyWith(
+                              fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(width: 8.0),
+                        MyIconButton(
+                          icon: arrowDownIcon,
+                          onTap: () {
+                            _showAddressBottomSheet(context);
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8.0),
+                    TextField(
                       controller: _addressController,
-                      errorText: state.userAddressError,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: const BorderSide(
+                              color: grayBorderColor, width: 1.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide:
+                              const BorderSide(color: primaryColor, width: 1.0),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide:
+                              const BorderSide(color: alertColor, width: 1.0),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide:
+                              const BorderSide(color: alertColor, width: 1.0),
+                        ),
+                        errorText: state.userAddressError,
+                      ),
+                      maxLines: 2,
                     ),
                     const SizedBox(height: 30),
                     Text(
@@ -136,7 +175,8 @@ class _CreateOrderPageState extends State<CreateOrderPage> {
                       ),
                     ),
                     const SizedBox(height: 20.0),
-                    if (state.addedListProduct != null && state.addedListProduct!.isNotEmpty)
+                    if (state.addedListProduct != null &&
+                        state.addedListProduct!.isNotEmpty)
                       ExpandProduct(
                         cart: state.addedListProduct,
                         onDelete: (value) {
@@ -663,6 +703,35 @@ class _CreateOrderPageState extends State<CreateOrderPage> {
                 ),
         ),
       ],
+    );
+  }
+
+  void _showAddressBottomSheet(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 16.0),
+          child: ListView.separated(
+            separatorBuilder: (context, index) => const Divider(color: primaryColor,),
+            itemCount: SessionData.mine!.addresses.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    _addressController.text = SessionData.mine!.addresses[index].address.toString();
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    SessionData.mine!.addresses[index].address.toString(),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
